d5a90ac65d51152c2396916b90fa5f5595bdc7dc
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2020-05-20 17:39:25 -0700

    erase credentials, data manipulation

--- notebooks/yvonne-covid.py
+++ notebooks/yvonne-covid.py
@@ -5,15 +5,11 @@ dbutils.fs.unmount("/mnt/")
 # COMMAND ----------
 
 # Session Configuration
-app_id = "cf586367-1ec1-4c0c-8cca-6c1446ff27a0"
-secret = "1ebcb5a1-340b-4c5d-9a4d-68663f7fb6f3"
-tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-
 configs = {"fs.azure.account.auth.type": "OAuth",
        "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-       "fs.azure.account.oauth2.client.id": "cf586367-1ec1-4c0c-8cca-6c1446ff27a0",
-       "fs.azure.account.oauth2.client.secret": "1ebcb5a1-340b-4c5d-9a4d-68663f7fb6f3",
-       "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token",
+       "fs.azure.account.oauth2.client.id": "*****",
+       "fs.azure.account.oauth2.client.secret": "*****",
+       "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/*****/oauth2/token",
        "fs.azure.createRemoteFileSystemDuringInitialization": "true"}
 
 # Mount container
@@ -30,14 +26,20 @@ display(dbutils.fs.ls("/mnt/"))
 # COMMAND ----------
 
 # Explore data
-patient = spark.read.json("mnt/fhir_patient_data/Patient.ndjson")
-display(patient)
+patient_df = spark.read.json("mnt/fhir_patient_data/Patient.ndjson")
+display(patient_df)
 
 # COMMAND ----------
 
 # Additional Data Exploration
-condition = spark.read.json("mnt/fhir_patient_data/Condition.ndjson")
-display(condition)
+condition_df = spark.read.json("mnt/fhir_patient_data/Condition.ndjson")
+display(condition_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import regexp_extract, col
+condition_df2 = condition_df.withColumn('patient id', regexp_extract(col('subject'), '\/(.*)\]', 1))
+condition_df2.select('patient id', 'code').show(100,truncate=False)
 
 # COMMAND ----------
 
